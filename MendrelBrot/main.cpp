0c3566b935a386e2444eaf57b52214b06540ab5d
Jeremy <jsow1997@gmail.com>
2020-07-01 22:37:59 +1000

    SIMD slightly incorrect

--- MendrelBrot/main.cpp
+++ MendrelBrot/main.cpp
@@ -63,17 +63,34 @@ public:
 		const double ScaleY = (FractalTL.y - FractalBR.y) / (ScreenTL.y - ScreenBR.y);
 		
 		//set up scale vector
-		__m256d ScaleXVector = _mm256_setr_pd(ScaleX, ScaleX, ScaleX, ScaleX);
-		__m256d ScaleYVector = _mm256_setr_pd(ScaleY, ScaleY, ScaleY, ScaleY);
+		__m256d ScaleXVector = _mm256_set1_pd(ScaleX);
+		__m256d ScaleYVector = _mm256_set1_pd(ScaleY);;
 
 		//fractal top left vector
 		__m256d FractalTLVectorY = _mm256_set1_pd(FractalTL.y);
 		__m256d FractalTLVectorX = _mm256_set1_pd(FractalTL.x);
 
-		__m256d CImVector, CReVector, ZImVector, ZReVector , ZTempVector, two, four;
+		__m256d CImVector, CReVector, ZImVector, ZReVector, ZTempVector;
+		__m256d ZImSquared, ZReSquared;
+		
+		/*vector to store comparison results.*/
+		__m256d cmp_cond_1;
+		__m256i cmp_cond_2;
+		__m256i CounterVector;		//counter vector
 
+		int* getCounters;
+		int* getIterations;
+		
+		__m256d ZMagnitude;	//ZMagnitude t check for bounds
+
+		__m256d two, four;	//constant vectors
+		__m256i one, maxiterationsVector, IterationVector;
+
+		/*initialize constants*/
 		two = _mm256_set1_pd(2.0);		//for computing fractal
 		four = _mm256_set1_pd(4.0);		//to check while loop condition.
+		one = _mm256_set1_epi64x(1);
+		maxiterationsVector = _mm256_set1_epi64x(maxiterations);
 
 		__m256d YValue, XValue;
 
@@ -82,40 +99,37 @@ public:
 		{
 			for (int y = ScreenTL.y; y < ScreenBR.y; y += 4)
 			{
-				/*what we CAN do , is compute 4 doubles at ONE go with mm256. we could calcualte for 4 complex numbers at a time*/
+				/*what we CAN do , is compute 4 doubles at ONE go with mm256. we could calculate for 4 complex numbers at a time*/
 				
-				
-
 				/*C part*/
-				double CReal = (x * ScaleX) + FractalTL.x;
-				double CIm = (y * ScaleY) + FractalTL.y;
-
+				
+				//double CIm = (y * ScaleY) + FractalTL.y;
 				/*imaginary C computation*/
 				YValue = _mm256_setr_pd(y, y + 1, y + 2, y + 3);
 				CImVector = _mm256_mul_pd(YValue, ScaleXVector);
-				CImVector = _mm256_add_pd(YValue, FractalTLVectorY);
+				CImVector = _mm256_add_pd(CImVector, FractalTLVectorY);
 
+				//double CReal = (x * ScaleX) + FractalTL.x;
 				/*Real C computation*/
 				XValue = _mm256_set1_pd((double)x);
 				CReVector = _mm256_mul_pd(XValue, ScaleXVector);
-				CReVector = _mm256_add_pd(XValue, FractalTLVectorX);
+				CReVector = _mm256_add_pd(CReVector, FractalTLVectorX);
 
-				double ZReal = 0.0;
-				double ZIm = 0.0;
+				//double ZReal = 0.0;
+				//double ZIm = 0.0;
 				/*initilaize Z Vector*/
 				ZReVector = _mm256_set1_pd(0.0);
 				ZImVector = _mm256_set1_pd(0.0);
 
-				int iteration_counter = 0;
+				//int iteration_counter = 0;
+				IterationVector = _mm256_set1_epi64x(0);
 
-				while ((ZReal*ZReal + ZIm * ZIm) < 4.0 && iteration_counter < maxiterations)
-				{
+			repeat:
 					/*F(Z) = Z^2  + C*/
-
 					//double ZTemp = (ZReal * ZReal) - (ZIm * ZIm) + CReal;
 					ZTempVector = _mm256_mul_pd(ZReVector , ZReVector);
 					ZTempVector = _mm256_sub_pd(ZTempVector, _mm256_mul_pd(ZImVector, ZImVector));
-					ZTempVector = _mm256_sub_pd(ZTempVector, CReVector);
+					ZTempVector = _mm256_add_pd(ZTempVector, CReVector);
 					
 					//ZIm = 2 * (ZIm * ZReal) + CIm;
 					ZImVector = _mm256_mul_pd(ZImVector , ZReVector);
@@ -124,11 +138,42 @@ public:
 					//ZReal = ZTemp;
 					ZReVector = ZTempVector;
 
-					
-					++iteration_counter;
-				}
+					//Z real squared and Z im squared vecotr
+					ZReSquared = _mm256_mul_pd(ZReVector, ZReVector);
+					ZImSquared = _mm256_mul_pd(ZImVector, ZImVector);
 
-				IterationStore[x + ScreenWidth() * y] = iteration_counter;
+					/*Zre^2 + Zim^2*/
+					ZMagnitude = _mm256_add_pd(ZReSquared , ZImSquared);
+					
+					/*Zre^2 + Zim^2 < 4.0*/
+					cmp_cond_1 = _mm256_cmp_pd(ZMagnitude,four,_CMP_LT_OQ);
+
+					/*maxiterations > \iterations*/
+					cmp_cond_2 = _mm256_cmpgt_epi64(maxiterationsVector,IterationVector);
+
+					/*condition1 && condition 2*/
+					cmp_cond_2 = _mm256_and_si256(cmp_cond_2, _mm256_castpd_si256(cmp_cond_1));
+
+					/*increment iteration ONLY IF the condition for a pixel is true*/
+					CounterVector = _mm256_and_si256(cmp_cond_2, one);
+
+					/*add 1 to the iteration counter*/
+					//++iteration_counter;
+					IterationVector = _mm256_add_epi64(IterationVector, CounterVector);
+
+					//continue the while loop if any outputs of the counter are not zero.
+					getCounters = (int*)&(CounterVector);
+					if (!(getCounters[0] == 0 && getCounters[1] == 0 && getCounters[2] == 0 && getCounters[2] == 0))
+					{
+						goto repeat;
+					}
+					else {
+						getIterations = (int*)&(IterationVector);
+						IterationStore[x + ScreenWidth() * y] = getIterations[3];
+						IterationStore[x + ScreenWidth() * (y + 1)] = getIterations[2];
+						IterationStore[x + ScreenWidth() * (y + 2)] = getIterations[1];
+						IterationStore[x + ScreenWidth() * (y + 3)] = getIterations[0];
+					}
 			}
 		}
 	}
@@ -151,8 +196,8 @@ public:
 	bool OnUserUpdate(float fElapsedTime) override
 	{
 		olc::vd2d GetMousePos = { (double)GetMouseX() , (double)GetMouseY() };
-		
-	
+
+
 		if (GetKey(olc::Key::A).bPressed)
 		{
 			MouseStartPos = GetMousePos;
@@ -169,7 +214,7 @@ public:
 		//screen coordiantes to world coordinates
 		olc::vd2d MouseBeforeZoom;
 		ScreenToWorld(GetMousePos, MouseBeforeZoom);
-		
+
 		/*zoom in*/
 		if (GetKey(olc::Key::Q).bHeld)
 		{
@@ -181,12 +226,12 @@ public:
 		{
 			Scale *= 0.8f;
 		}
-		
+
 		olc::vd2d MouseAfterZoom;
 		ScreenToWorld(GetMousePos, MouseAfterZoom);
-		
+
 		olc::vd2d GetOffSetVector = MouseAfterZoom - MouseBeforeZoom;
-	
+
 		OffSet -= GetOffSetVector;
 
 		if (GetKey(olc::Key::UP).bPressed)
@@ -197,9 +242,9 @@ public:
 		{
 			maxiterations -= 100;
 		}
-		
 
-	   //top left and bottom right of screenspace
+
+		//top left and bottom right of screenspace
 		olc::vd2d ScreenTL = { 0 , 0 };
 		olc::vd2d ScreenBR = { 1280, 720 };
 
@@ -211,20 +256,40 @@ public:
 		ScreenToWorld(ScreenTL, FractalTL);
 		ScreenToWorld(ScreenBR, FractalBR);
 
+		if (GetKey(olc::Key::Z).bPressed)
+		{
+			SimulationType += 1;
+			if (SimulationType == 2)
+			{
+				SimulationType = 0;
+			}
+		}
+
 		//compute performance
 		auto StartTime = std::chrono::high_resolution_clock::now();
+		switch (SimulationType)
+		{
+			case 0:
+				SimulationTypeString = "naive algorithm";
+				ComputeFractal(ScreenTL, ScreenBR, FractalTL, FractalBR, maxiterations);
+				break;
 
-		ComputeFractal(ScreenTL, ScreenBR, FractalTL, FractalBR, maxiterations);
+			case 1:
+				SimulationTypeString = "SIMD";
+				ComputeFractalSIMD(ScreenTL, ScreenBR, FractalTL, FractalBR, maxiterations);
+				break;
+		}
 
 		auto EndTime = std::chrono::high_resolution_clock::now();
 		
 		std::chrono::duration<double> TimeTaken = EndTime - StartTime;
 
-		//std::cout << TimeTaken.count() << std::endl;
 
 		//render the fractal (not included in computation)
 		RenderFractal();
-		
+
+		//print UI
+		DrawString(0, 0, "Simulation Type: " + SimulationTypeString, olc::BLACK, 3);
 		DrawString(0, 30, "Time Taken: " + std::to_string(TimeTaken.count()) + "s", olc::BLACK, 3);
 		DrawString(0, 60, "Iterations: " + std::to_string(maxiterations), olc::BLACK, 3);
 	
@@ -235,6 +300,9 @@ private:
 	olc::vd2d MouseStartPos;
 	olc::vd2d Scale = { 1280.0 * 0.5 , 720.0 };
 	olc::vd2d OffSet = { 0.0 , 0.0 };
+
+	int SimulationType = 0;
+	std::string SimulationTypeString = "naive algorithm";
 
 	int maxiterations = 50;
 
